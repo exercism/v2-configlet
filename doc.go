3766336dfe722c9db337dc7ac829d6a823aa96a7
Katrina Owen <katrina.owen@gmail.com>
2014-06-17 22:48:55 -0700

    Add package documentation

---
+++ doc.go
@@ -0,0 +1,25 @@
+/*
+Command configlet sanity-checks Exercism track configuration.
+
+A track consists of a collection of problems in a single language. Each
+problem has a test suite, an example solution, and any supporting files that
+are necessary to solve the problem.
+
+The configuration serves two purposes.
+
+1. It helps implementors ensure that all the problems are valid.
+2. It provides the API with a sort order for delivering problems.
+
+Run the command with
+
+    $ ./configlet path/to/track
+
+The binary is cross-compiled for a number of platforms, and is distributed
+with each track repository. The repositories are located at
+github.com/exercism/x<TRACK>.
+
+The command also runs on Travis-CI.
+
+See http://exercism.io for more information about the project.
+*/
+package main
