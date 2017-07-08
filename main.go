ba4fb977cee3bc44e5ef7b01cb37ff6ed58b07a1
Katrina Owen <kytrinyx@github.com>
2017-07-08 17:14:46 -0600

    Bump version to v2.0.2

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ type ConfigError struct {
 const (
 	// Version represents the latest released version of the project.
 	// Configlet follows semantic versioning.
-	Version = "2.0.1"
+	Version = "2.0.2"
 )
 
 var showVersion = flag.Bool("version", false, "output the version of the tool")
