3d1d6cafa11422ae49e9bf94d1778c9e3e02f6d8
Rob Phoenix <rob@robphoenix.com>
2017-08-07 23:07:04 +0100

    Minor changes to lint & generate commands (#45)
    * Add a usage function to generate command
    
    * Add a usage function to lint command
    
    * use filepath.FromSlash in lint_example_test.go

--- cmd/generate.go
+++ cmd/generate.go
@@ -14,57 +14,68 @@ var (
 	specPath string
 )
 
-// generateCmd represents the generate command
-var generateCmd = &cobra.Command{
-	Use:   "generate",
-	Short: "Generate exercise READMEs for an Exercism language track",
-	Long: `Generate READMEs for Exercism exercises based on contents of
-	a number of different files.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) == 0 {
-			fmt.Fprintf(os.Stderr, "USAGE: configlet generate path/to/track")
-			os.Exit(1)
-		}
+var (
+	// generateCmd represents the generate command
+	generateCmd = &cobra.Command{
+		Use:   "generate",
+		Short: "Generate exercise READMEs for an Exercism language track",
+		Long: `Generate READMEs for Exercism exercises based on contents of
+a number of different files.`,
+		Run: generate,
+	}
+	generateUsageText = "USAGE: configlet generate <path/to/track>\n"
+)
 
-		path, err := filepath.Abs(filepath.FromSlash(args[0]))
+func generate(cmd *cobra.Command, args []string) {
+	if len(args) == 0 {
+		generateUsageFunc(cmd)
+		os.Exit(1)
+	}
+
+	path, err := filepath.Abs(filepath.FromSlash(args[0]))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	track.ProblemSpecificationsPath = specPath
+	root := filepath.Dir(path)
+	trackID := filepath.Base(path)
+
+	var exercises []track.Exercise
+
+	if genSlug != "" {
+		exercises = append(exercises, track.Exercise{Slug: genSlug})
+	} else {
+		track, err := track.New(path)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, err.Error())
 			os.Exit(1)
 		}
+		exercises = track.Exercises
+	}
 
-		track.ProblemSpecificationsPath = specPath
-		root := filepath.Dir(path)
-		trackID := filepath.Base(path)
-
-		var exercises []track.Exercise
-
-		if genSlug != "" {
-			exercises = append(exercises, track.Exercise{Slug: genSlug})
-		} else {
-			track, err := track.New(path)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, err.Error())
-				os.Exit(1)
-			}
-			exercises = track.Exercises
+	errs := []error{}
+	for _, exercise := range exercises {
+		readme, err := track.NewExerciseReadme(root, trackID, exercise.Slug)
+		if err != nil {
+			errs = append(errs, err)
+			continue
 		}
-
-		errs := []error{}
-		for _, exercise := range exercises {
-			readme, err := track.NewExerciseReadme(root, trackID, exercise.Slug)
-			if err != nil {
-				errs = append(errs, err)
-				continue
-			}
-			if err := readme.Write(); err != nil {
-				errs = append(errs, err)
-			}
+		if err := readme.Write(); err != nil {
+			errs = append(errs, err)
 		}
-	},
+	}
+}
+
+func generateUsageFunc(cmd *cobra.Command) error {
+	fmt.Fprintf(os.Stderr, generateUsageText)
+	return nil
 }
 
 func init() {
 	RootCmd.AddCommand(generateCmd)
+	generateCmd.SetUsageFunc(generateUsageFunc)
 	generateCmd.Flags().StringVarP(&genSlug, "only", "o", "", "Generate READMEs for just the exercise specified (by the slug).")
 	generateCmd.Flags().StringVarP(&specPath, "spec-path", "p", "", "The location of the problem-specifications directory.")
 }
